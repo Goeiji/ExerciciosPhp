cd2c0f2ed773c5edd8a9c56394fe785ab9b8ad2c
Goeiji <53585549+Goeiji@users.noreply.github.com>
2019-08-16 13:54:16 -0300

    Update Resolucao.php

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -5,6 +5,19 @@ namespace Galoa\ExerciciosPhp\TextWrap;
 /**
  * Implemente sua resolução aqui.
  */
+    <?php
+        function quebra(&$texto, &$comprim){
+            $caract = explode(" ", $texto, $comprim);
+            for($i = 0; $i < $comprim; $i++){
+                echo "$caract[$i]<br/>";
+            }
+        }
+
+        $text = 'Define uma interface para o exercício de quebra de linha.';
+        $length = 2;
+        quebra($text,$length);
+    ?>
+
 class Resolucao implements TextWrapInterface {
 
   /**
